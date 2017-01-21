ed22aa0706c712dcf9a29b15e6123ba03b0804d8
Jamil Nawaz <jamilnawaz88@gmail.com>
2017-01-21 13:57:37 +0000

    Added assertions to calculate method
    Assertions ensure the calculate method of JNLoanCalculator can be used
    properly.

--- JNFinanceCalculator/JNFinanceCalculator/JNLoanCalculator/JNLoanCalculator.swift
+++ JNFinanceCalculator/JNFinanceCalculator/JNLoanCalculator/JNLoanCalculator.swift
@@ -10,9 +10,9 @@ import Foundation
 
 public class JNLoanCalculator {
 
-    var loanAmount: Double?
-    var apr: Double?
-    var loanTerm: Int? // in Months
+    var loanAmount: Double = 0.0
+    var apr: Double = 0.0
+    var loanTerm: Int = 0 // in Months
 
     var monthlyRepayment: Double?
     var totalInterest: Double?
@@ -20,6 +20,9 @@ public class JNLoanCalculator {
 
 
     func calculate() {
+        assert(Double(loanAmount) > 0.0, "Loan amount has to be greater than 0.00")
+        assert(Int(loanTerm) > 0, "Loan term has to be greater than 0 months")
+
 
     }
 }
