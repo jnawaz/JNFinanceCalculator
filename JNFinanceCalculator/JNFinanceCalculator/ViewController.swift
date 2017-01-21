61fedc3a4c80a3d4acbb89904013e7edf476c770
Jamil Nawaz <jamilnawaz88@gmail.com>
2017-01-21 09:53:15 +0000

    Added iOS project and gitignore.

---
+++ JNFinanceCalculator/JNFinanceCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JNFinanceCalculator
+//
+//  Created by Jamil Nawaz on 21/01/2017.
+//  Copyright (c) 2017 Jamil Nawaz. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
